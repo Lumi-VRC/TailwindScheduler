36784db8bc57f83c8366db0bb2f0a0367d5e0a2d
Prince Lumine <34037661+Lumi-VRC@users.noreply.github.com>
2025-04-10 08:30:35 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
